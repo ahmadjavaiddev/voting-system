ab6a92e968d0ac9f842cd45860633a24afab598b
Ahmad Javaid Dev <ahmadjavaiddev@gmail.com>
2025-05-21 02:37:16 +0500

    Enhance :: updated container width on ElectionDetails page for improved layout

--- src/app/admin/election/[id]/page.jsx
+++ src/app/admin/election/[id]/page.jsx
@@ -28,7 +28,7 @@ const ElectionDetails = () => {
   }
 
   return (
-    <div className="container mx-auto p-4 md:p-6 max-w-5xl">
+    <div className="container mx-auto p-4 md:p-6 max-w-6xl">
       <Button
         variant="ghost"
         size="sm"
