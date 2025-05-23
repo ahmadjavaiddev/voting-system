d440f5b5a35d247d2b17874bc757faaea423dbbe
Ahmad Javaid Dev <ahmadjavaiddev@gmail.com>
2025-05-24 02:32:31 +0500

    Refactor :: Removed moment.js dependency and simplified election status display logic in SelectedElection component, enhancing performance and reducing complexity.

--- src/components/general/results/SelectedElection.jsx
+++ src/components/general/results/SelectedElection.jsx
@@ -5,7 +5,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import moment from "moment";
 import React from "react";
 import ElectionStats from "./ElectionStats";
 import ResultsVisualization from "./ResultsVisualization";
@@ -20,25 +19,6 @@ const SelectedElection = ({
     <div className="lg:col-span-2 space-y-6">
       {selectedElection && (
         <div className="lg:col-span-2 space-y-6 sticky top-16">
-          {(() => {
-            const now = moment();
-            const end = moment(selectedElection.endTime);
-            if (!selectedElection.endTime) return null;
-            if (now.isAfter(end)) {
-              return (
-                <div className="mb-2 text-red-500 font-semibold">
-                  Election Ended {end.fromNow()} (ended at {end.format("LLL")})
-                </div>
-              );
-            } else {
-              return (
-                <div className="mb-2 text-green-600 font-semibold">
-                  Time Remaining: {end.toNow(true)} (ends at {end.format("LLL")}
-                  )
-                </div>
-              );
-            }
-          })()}
           <Card>
             <CardHeader>
               <div className="flex justify-between items-start">
